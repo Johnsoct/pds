75e64c5d715c4a1fcad53806f88bd885c568330d
Taylor <johnsoct@hey.com>
2025-05-05 09:59:07 -0400

    Converted constant class into a singleton

--- constants.py
+++ constants.py
@@ -1,7 +1,14 @@
 # Packages
+import re
 from dataclasses import dataclass
 
+# SINGLETON - import C
 @dataclass(frozen=True)
 class Constants:
     FREQUENCIES: tuple[str, ...] = ("bi-monthly", "bi-weekly", "monthly", "weekly", "yearly")
     DISALLOWED_DANGEROUS_CHARACTERS: tuple[str, ...] = (";", "&&", "||", "|", "(", ")", "`", ">", ">>", "<", "*", "?", "~", "$", ",", "%")
+
+    def get_disallowed_dangerous_characters_regex(self):
+        return f"[{''.join(re.escape(c) for c in self.DISALLOWED_DANGEROUS_CHARACTERS)}]"
+
+C = Constants()
