52322405775223f294db0caee83eea0a9ea377ba
Taylor <johnsoct@hey.com>
2025-05-06 19:26:45 -0400

    Added additional contribution step and tests

--- input.py
+++ input.py
@@ -7,28 +7,133 @@ additional_contribution_amount = 0
 additional_contribution_frequency = 'monthly'
 debts = [] # loan_amount, interest_rate, term_length, current_balance
 
+def collect_additional_contribution_information(testing = False):
+    print()
+
+    additional_contribution_frequency = collect_input(
+        "frequency",
+        "How often do you want to contribute an additional amount to your debt? ",
+        testing = testing
+    )
+    additional_contribution_amount = collect_input(
+        "numerical",
+        f"How much would you like to contribute every {additional_contribution_frequency}? ",
+        testing = testing
+    )
+
+    return additional_contribution_frequency, additional_contribution_amount
+
 def collect_debt_information():
-    current_balance = collect_input("numerical", "What is the current balance of the loan?")
-    interest_rate = collect_input("numerical", "What is the interest rate of the loan?")
-    loan_amount = collect_input("numerical", "What was the original amount of the loan?")
-    term_length = collect_input("numerical", "What is the term length of the loan?")
+    print()
+    current_balance = collect_input("numerical", "What is the current balance of the loan? ")
+    interest_rate = collect_input("numerical", "What is the interest rate of the loan? ")
+    loan_amount = collect_input("numerical", "What was the original amount of the loan? ")
+    term_length = collect_input("numerical", "What is the term length of the loan? ")
 
     return current_balance, interest_rate, loan_amount, term_length
 
-def collect_input(type, prompt):
+def confirm_additional_contribution_information(frequency, amount, testing = False):
+    print()
+    display_additional_contribution_information((frequency, amount))
+    print("---------------------------------------")
+    print("Does this information look correct?")
+    print("---------------------------------------")
+    print("If 'NO', you'll be asked to enter the information again.")
+    print("If 'YES', you'll move on to calculating your amortization schedule.")
+
+    user_input_confirmation = collect_input("confirmation", testing = testing)
+
+    return get_user_confirmation_comparison(user_input_confirmation)
+
+def confirm_additional_contribution_intent(testing = False):
+    print()
+    print(f"Do you intend to contribute an additional amount towards your debt every {C.FREQUENCIES}")
+    print("---------------------------------------")
+    print("If 'YES', I'll ask you how much and how often")
+    print("If 'NO', I'll continue to calculating your debt's amortization schedule")
+    print("---------------------------------------")
+
+    additional_contribution_confirmation = collect_input("confirmation", testing = testing)
+
+    return get_user_confirmation_comparison(additional_contribution_confirmation)
+
+def confirm_another_debt(testing = False):
+    print()
+    print("Would you like to enter another debt?")
+    print("---------------------------------------")
+    print("If 'NO', you'll move on towards getting your debt's amortization schedule.")
+    print("If 'YES', you'll be asked to enter another debt's information.")
+
+    user_input_confirmation = collect_input("confirmation", testing = testing)
+
+    return get_user_confirmation_comparison(user_input_confirmation)
+
+def confirm_debt_information(current_balance, interest_rate, loan_amount, term_length, testing = False):
+    print()
+    display_debt_information((current_balance, interest_rate, loan_amount, term_length))
+    print("---------------------------------------")
+    print("Does this information look correct?")
+    print("---------------------------------------")
+    print("If 'NO', you'll be asked to enter the information again.")
+    print("If 'YES', you'll move on to adding additional debts, if any.")
+
+    user_input_confirmation = collect_input("confirmation", testing = testing)
+
+    return get_user_confirmation_comparison(user_input_confirmation)
+
+def collect_input(type, prompt = "", testing = False):
+    options = get_options(type)
     user_input = input(prompt)
-    normalized_user_input = normalize_user_input(user_input, C.get_disallowed_dangerous_characters_regex())
+    user_input_normalized = normalize_user_input(
+        user_input,
+        C.get_disallowed_dangerous_characters_regex()
+    )
 
-    if not validate_input(type, normalized_user_input, C.FREQUENCIES):
-        print(f"{user_input} was not valid")
-        collect_input(type, prompt)
+    if not validate_input(type, user_input_normalized, options):
+        print(f"{user_input!r} was not valid")
+        
+        # NOTE: When testing failed cases, we want to avoid infinite recursion
+        if not testing:
+            return collect_input(type, prompt)
+    else:
+        return user_input_normalized 
 
-    return normalized_user_input 
+def display_additional_contribution_information(additional_contribution_information):
+    print("Here is the information you entered:\n")
+    print(f"Frequency: {additional_contribution_information[0]}")
+    print(f"Amount: ${additional_contribution_information[1]}")
+
+def display_debt_information(debt_information):
+    print("Here is the information you entered:\n")
+    print(f"Current balance: ${debt_information[0]}")
+    print(f"Interest rate: {debt_information[1]}%")
+    print(f"Original loan amount: ${debt_information[2]}")
+    print(f"Term length: {debt_information[3]} months")
+
+def get_options(type):
+    if type == "frequency":
+        return C.FREQUENCIES
+    elif type == "confirmation":
+        return C.CONFIRMATIONS
+    else:
+        return []
+
+def get_user_confirmation_comparison(user_input_confirmation):
+    # WARN: do not call this before normalize_user_input() and validate_input()
+    # NOTE: I do not want to overcomplicated the C.CONFIRMATIONS value type
+    # because validate_input_option_in_options would get unnecessarily complicated
+    if user_input_confirmation and "y" in user_input_confirmation:
+        return True
+    else:
+        return False
 
 def introduce_user_to_process():
     print("I'm going to ask you a series of questions about your debts and any additional contributions you want to make towards your monthly debt. Each debt will require the original loan amount, interest rate, term length, and the current balance.")
     print("")
-    print("When entering numerical values, such as dollars or percents, do not use special characters, such as $ or commas, and express percents as decimals, such as 3.25 instead of 0.0325")
+    print("When entering numerical values, such as dollars or percents, do not use special characters, such as $ or commas, and express percents as decimals, such as 3.25 instead of 0.0325\n")
+
+def is_float_positive(float):
+    return float >= 0
 
 def normalize_user_input(input, replace_pattern):
     if isinstance(input, str):
@@ -36,13 +141,74 @@ def normalize_user_input(input, replace_pattern):
     else:
         raise TypeError(f"{input!r} is not a string; cannot normalize")
 
+def step_collect_additional_contribution():
+    additional_contribution_confirmation = confirm_additional_contribution_intent()
+
+    if additional_contribution_confirmation:
+        additional_contribution_information = collect_additional_contribution_information()
+        user_confirmation = confirm_additional_contribution_information(*additional_contribution_information)
+
+        if user_confirmation:
+            print()
+            print()
+            print()
+            print("Here is the debt information we collected:\n")
+            print("---------------------------------------")
+            display_additional_contribution_information(additional_contribution_information)
+
+            return additional_contribution_information
+        else:
+            return step_collect_additional_contribution()
+    else:
+        print()
+        print()
+        print()
+        print("Skipping additional contributions...")
+        
+        return None
+
+
+def step_collect_debts():
+    debts = []
+    is_user_finished_submitting = False
+
+    while not is_user_finished_submitting:
+        debt_information = collect_debt_information()
+        user_confirmation = confirm_debt_information(*debt_information)
+
+        if user_confirmation:
+            debts.append(debt_information)
+
+            user_confirmation = confirm_another_debt()
+
+            if user_confirmation:
+                continue
+            else:
+                is_user_finished_submitting = True
+        else: 
+            # NOTE: Effectively, this is recursively calling collect_debt_information()
+            continue
+
+    print()
+    print()
+    print()
+    print("Here is the debt information we collected:\n")
+
+    for debt in debts:
+        print()
+        print(f"Debt #{debts.index(debt)}")
+        print("---------------------------------------")
+        display_debt_information(debt)
+
+    return debts
+
 def strip_dangerous_characters_from_user_input(pattern, input):
     if isinstance(input, str):
         return re.sub(pattern, "", input)
     else:
         raise TypeError(f"{input!r} is not a string; cannot strip")
 
-def validate_input_frequency(input, options):
+def validate_input_option_in_options(input, options):
     if input in options:
         return True
     else:
@@ -50,35 +216,33 @@ def validate_input_frequency(input, options):
 
 def validate_input_numerical(input):
     try:
-        float(input)
-        return True
-    except (TypeError, ValueError):
+        if is_float_positive(float(input)):
+            return True
+        else:
+            print(f"{float(input)!r} was negative")
+            return False
+    except:
         print(f"{input!r} could not be converted to a decimal")
         return False
 
-def validate_input_percentage(input):
-    if float(input) >= 0:
-        return True
-    else:
-        return False
-
-def validate_input(type, normalized_user_input, options):
+def validate_input(type, user_input_normalized, options):
     valid = True
 
-    if type == "frequency":
-        valid = validate_input_frequency(normalized_user_input, options)
+    if type in ["confirmation", "frequency"]:
+        valid = validate_input_option_in_options(user_input_normalized, options)
     elif type == "numerical":
-        valid = validate_input_numerical(normalized_user_input)
-    elif type == "percentage":
-        valid = validate_input_percentage(normalized_user_input)
+        valid = validate_input_numerical(user_input_normalized)
 
     return valid
     
 def main():
     introduce_user_to_process()
-    loan1 = collect_debt_information()
 
-    print(loan1)
+    debts = step_collect_debts() 
+    additional_contribution = step_collect_additional_contribution()
+
+    print(debts)
+    print(additional_contribution)
 
 if __name__ == "__main__":
     main()
--- input_test.py
+++ input_test.py
@@ -1,14 +1,236 @@
 # Packages
 import pytest
+import sys
 # Modules
-from input import normalize_user_input, strip_dangerous_characters_from_user_input, validate_input, validate_input_frequency, validate_input_numerical, validate_input_percentage
+from input import collect_additional_contribution_information, collect_input, confirm_additional_contribution_information, confirm_additional_contribution_intent, confirm_another_debt, confirm_debt_information, get_options, get_user_confirmation_comparison, is_float_positive, normalize_user_input, strip_dangerous_characters_from_user_input, validate_input, validate_input_option_in_options, validate_input_numerical, validate_input_option_in_options
 # Constants
 from constants import C 
 
 class TestValidateInput:
-    def collect_input(self):
-        # TODO: the stored input value should update if the function recurses
-        pass
+    def test_collect_additional_contribution_information(self, capsys, monkeypatch):
+        failing_tests = [
+            ("bimonthly", "100"),
+            ("biweekly", "100"),
+            ("monthly", "abc"),
+            ("weekly", "abc"),
+        ]
+        passing_tests = [(frequency, "100") for frequency in C.FREQUENCIES]
+        recursively_passing_tests = [
+            [("bimonthly", "bi-monthly"), ("abc", "100")],
+            [("biweekly", "bi-weekly"), ("abc", "100")],
+            [("month", "monthly"), ("abc", "100")],
+            [("week", "weekly"), ("abc", "100")],
+            [("year", "yearly"), ("abc", "100")],
+        ]
+
+        for test in failing_tests:
+            tests = iter(test)
+            monkeypatch.setattr("builtins.input", lambda _: next(tests))
+
+            collect_additional_contribution_information(testing = True)
+
+            # Prepare the stdout messages to assert against
+            collect_input_stdout = f"was not valid"
+            test_stdout = capsys.readouterr().out
+            
+            assert f"{collect_input_stdout}" in f"{test_stdout}"
+
+        for test in passing_tests:
+            tests = iter(test)
+
+            monkeypatch.setattr("builtins.input", lambda _: next(tests))
+
+            assert collect_additional_contribution_information() == (test[0], test[1])
+
+        for test in recursively_passing_tests:
+            tests = iter([*test[0], *test[1]])
+
+            monkeypatch.setattr("builtins.input", lambda _: next(tests))
+
+            assert collect_additional_contribution_information() == (test[0][1], test[1][1])
+
+    def test_collect_input(self, capsys, monkeypatch):
+        failing_tests = [
+            ("confirmation", "", "abc"),
+            ("confirmation", "", "noh"),
+            ("numerical", "What is the current balance of the loan?", "abc"),
+            ("numerical", "What is the interest rate of the loan?", "$12.00 cents"),
+            ("numerical", "What was the original amount of the loan?", "-12"),
+            ("numerical", "What is the term length of the loan?", "sixty months"),
+        ]
+        passing_tests = [
+            ("confirmation", "", "yes"),
+            ("confirmation", "", "y"),
+            ("confirmation", "", "YES"),
+            ("confirmation", "", "Yes"),
+            ("confirmation", "", "no"),
+            ("confirmation", "", "n"),
+            ("confirmation", "", "NO"),
+            ("confirmation", "", "No"),
+            ("numerical", "What is the current balance of the loan?", "9,000"),
+            ("numerical", "What is the interest rate of the loan?", "12"),
+            ("numerical", "What was the original amount of the loan?", "12,000"),
+            ("numerical", "What is the term length of the loan?", "60"),
+        ]
+        recursively_passing_tests = [
+            ("confirmation", "", ["yah", "yes"]),
+            ("confirmation", "", ["nah", "no"]),
+            ("numerical", "What is the current balance of the loan?", ["abc", "9,000"]),
+            ("numerical", "What is the interest rate of the loan?", ["$12.00 cents", "12"]),
+            ("numerical", "What was the original amount of the loan?", ["-12", "12,000"]),
+            ("numerical", "What is the term length of the loan?", ["sixty months", "60"]),
+        ]
+
+        for test in failing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test[2])
+
+            collect_input(test[0], test[1], testing = True)
+
+            # Prepare the stdout messages to assert against
+            collect_input_stdout = f"{test[2]!r} was not valid"
+            test_stdout = capsys.readouterr().out
+            
+            assert f"{collect_input_stdout}" in f"{test_stdout}"
+
+        for test in passing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test[2])
+
+            normalized_user_input = collect_input(test[0], test[1])
+
+            assert normalized_user_input == normalize_user_input(
+                test[2],
+                C.get_disallowed_dangerous_characters_regex()
+            )
+
+        for test in recursively_passing_tests:
+            tests = iter(test[2])
+
+            monkeypatch.setattr("builtins.input", lambda _: next(tests))
+
+            collect_input_output = collect_input(test[0], test[1])
+            normalized_user_input = normalize_user_input(
+                test[2][1],
+                C.get_disallowed_dangerous_characters_regex(),
+            )
+
+            assert collect_input_output == normalized_user_input
+
+    def test_confirm_additional_contribution_intent(self, capsys, monkeypatch):
+        failing_tests = ["yess", "yers", "yeh", "nah", "nay", "noo"]
+        passing_tests = [*C.CONFIRMATIONS]
+
+        for test in failing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test)
+
+            confirm_additional_contribution_intent(testing = True)
+            
+            # Prepare the stdout messages to assert against
+            confirm_additional_contribution_stdout = f"{test!r} was not valid"
+            test_stdout = capsys.readouterr().out
+            
+            assert f"{confirm_additional_contribution_stdout}" in f"{test_stdout}"
+
+        for test in passing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test)
+            
+            if "y" in test:
+                assert confirm_additional_contribution_intent()
+            else:
+                assert not confirm_additional_contribution_intent()
+
+    def test_confirm_another_debt(self, capsys, monkeypatch):
+        failing_tests = ["yess", "yers", "yeh", "nah", "nay", "noo"]
+        passing_tests = [*C.CONFIRMATIONS]
+
+        for test in failing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test)
+
+            confirm_another_debt(testing = True)
+            
+            # Prepare the stdout messages to assert against
+            confirm_another_debt_stdout = f"{test!r} was not valid"
+            test_stdout = capsys.readouterr().out
+            
+            assert f"{confirm_another_debt_stdout}" in f"{test_stdout}"
+
+        for test in passing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test)
+            
+            if "y" in test:
+                assert confirm_another_debt()
+            else:
+                assert not confirm_another_debt()
+
+    def test_confirm_additional_contribution_information(self, capsys, monkeypatch):
+        additional_contribution_information = ("monthly", "$100.00")
+        passing_tests = [*C.CONFIRMATIONS]
+
+        for test in passing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test)
+
+            confirm_additional_contribution_information(*additional_contribution_information)
+            
+            # Prepare the stdout messages to assert against
+            confirm_additional_contribution_information_stdout = f"Frequency: {additional_contribution_information[0]}\nAmount: ${additional_contribution_information[1]}\n---------------------------------------\nDoes this information look correct?\n---------------------------------------\nIf 'NO', you'll be asked to enter the information again.\nIf 'YES', you'll move on to calculating your amortization schedule.\n"           
+            test_stdout = capsys.readouterr().out
+            
+            assert f"{confirm_additional_contribution_information_stdout}" in f"{test_stdout}"
+
+            if "y" in test:
+                assert confirm_additional_contribution_information(*additional_contribution_information)
+
+            else:
+                assert not confirm_additional_contribution_information(*additional_contribution_information)
+
+    def test_confirm_debt_information(self, capsys, monkeypatch):
+        debt_information = ("10000.00", "12.5", "32000", "72")
+        passing_tests = [*C.CONFIRMATIONS]
+
+        for test in passing_tests:
+            monkeypatch.setattr("builtins.input", lambda _: test)
+
+            confirm_debt_information(*debt_information)
+            
+            # Prepare the stdout messages to assert against
+            confirm_debt_information_stdout = f"Current balance: ${debt_information[0]}\nInterest rate: {debt_information[1]}%\nOriginal loan amount: ${debt_information[2]}\nTerm length: {debt_information[3]} months\n---------------------------------------\nDoes this information look correct?\n---------------------------------------\nIf 'NO', you'll be asked to enter the information again.\nIf 'YES', you'll move on to adding additional debts, if any.\n"           
+            test_stdout = capsys.readouterr().out
+            
+            assert f"{confirm_debt_information_stdout}" in f"{test_stdout}"
+
+            if "y" in test:
+                assert confirm_another_debt()
+            else:
+                assert not confirm_another_debt()
+
+    def test_get_options(self):
+        assert get_options("confirmation") == C.CONFIRMATIONS
+        assert get_options("frequency") == C.FREQUENCIES
+        assert get_options("unknown") == []
+
+    def test_get_user_confirmation_comparison(self):
+        false_tests = ["n", "N", "no", "NO", "No"]
+        true_tests = ["y", "Y", "yes", "YES", "Yes"]
+
+        for test in false_tests:
+            assert not get_user_confirmation_comparison(
+                normalize_user_input(test, C.get_disallowed_dangerous_characters_regex())
+            )
+
+        for test in true_tests:
+            assert get_user_confirmation_comparison(
+                normalize_user_input(test, C.get_disallowed_dangerous_characters_regex())
+
+            )
+
+    def test_is_float_positive(self):
+        failing_tests = [-0.1, -2]
+        passing_tests = [0.0, 1, 12.23]
+
+        for test in failing_tests:
+            assert not is_float_positive(test)
+
+        for test in passing_tests:
+            assert is_float_positive(test)
 
     def test_normalize_user_input(self):
         passing_tests = [
@@ -63,6 +285,10 @@ class TestValidateInput:
 
     def test_validate_input(self):
         failling_tests = [
+            ("confirmation", "yers"),
+            ("confirmation", "yess"),
+            ("confirmation", "nah"),
+            ("confirmation", "nay"),
             ("frequency", "bimonthly"),
 			("frequency", "biweekly"),
 			("frequency", "tacos"),
@@ -99,29 +325,46 @@ class TestValidateInput:
 			("numerical", "$1"),
         ]
 
+        for confirmation in C.CONFIRMATIONS:
+            passing_tests.append(("confirmation", confirmation))
+
         for frequency in C.FREQUENCIES:
             passing_tests.append(("frequency", frequency))
 
         for test in failling_tests:
             normalized_user_input = normalize_user_input(test[1], C.get_disallowed_dangerous_characters_regex())
-            assert not validate_input(test[0], normalized_user_input, C.FREQUENCIES)
+            pattern = None
+
+            if test[0] == "frequency":
+                pattern = C.FREQUENCIES
+            else:
+                pattern = C.CONFIRMATIONS
+
+            assert not validate_input(test[0], normalized_user_input, pattern)
 
         for test in passing_tests:
             normalized_user_input = normalize_user_input(test[1], C.get_disallowed_dangerous_characters_regex())
-            assert validate_input(test[0], normalized_user_input, C.FREQUENCIES)
+            pattern = None
 
-    def test_validate_input_frequency(self):
-        failing_tests = ["bimonthly", "biweekly", "tacos", "month", "week", "bi-month", "bi-week", "year"]
-        passing_tests = C.FREQUENCIES
+            if test[0] == "frequency":
+                pattern = C.FREQUENCIES
+            else:
+                pattern = C.CONFIRMATIONS
+
+            assert validate_input(test[0], normalized_user_input, pattern)
+
+    def test_validate_input_option_in_options(self):
+        failing_tests = ["yess", "yers", "nah", "nay"]
+        passing_tests = C.CONFIRMATIONS
 
         for test in failing_tests:
-            assert not validate_input_frequency(test, C.FREQUENCIES)
+            assert not validate_input_option_in_options(test, C.CONFIRMATIONS)
 
         for test in passing_tests:
-            assert validate_input_frequency(test, C.FREQUENCIES)
+            assert validate_input_option_in_options(test, C.CONFIRMATIONS)
 
     def test_validate_input_numerical(self):
-        failing_tests = ["a", "!", "$", ".", ",", "%", "11,111.00", "1,000,000", "$1.00", "$1"]
+        failing_tests = ["a", "!", "$", ".", ",", "%", "11,111.00", "1,000,000", "$1.00", "$1", "-12"]
         passing_tests = [
             0, 0.1, 0.01, 1, 1.1, 1.11, 11, 11.1, 11111.00, 1000000,
             "0", "0.1", "0.01", "1", "1.1", "1.11", "11", "11.1"
@@ -132,14 +375,3 @@ class TestValidateInput:
 
         for test in passing_tests:
             assert validate_input_numerical(test)
-
-    def test_validate_input_percentage(self):
-        failing_tests = ["-1", "-0.25"]
-        passing_tests = ["0", "0.01", "100", "100.25"]
-
-        for test in failing_tests:
-            assert not validate_input_percentage(test)
-
-        for test in passing_tests:
-            assert validate_input_percentage(test)
-
