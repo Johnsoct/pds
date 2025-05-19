918d94ebfc800c32753caa028e4be41a33cb784e
Taylor <johnsoct@hey.com>
2025-05-18 20:00:57 -0400

    wip calculating amortization schedule; small improvements to existing tests

--- amortization.py
+++ amortization.py
@@ -1,21 +1,54 @@
 # Packages
-from tabulate import tabulate
+from decimal import Decimal, ROUND_HALF_UP
+# from tabulate import tabulate
 import math
 
 def calculate_amortization_schedule(debts_and_additional_contributions):
-    additional_contribution_amount = debts_and_additional_contributions["additional_contribution_amount"]
+    additional_contribution_amount = float(debts_and_additional_contributions["additional_contribution_amount"])
     additional_contribution_frequency = debts_and_additional_contributions["additional_contribution_frequency"]
     debts = sort_debts(debts_and_additional_contributions["debts"])
     schedule = []
 
-    # TODO: iterate through each month of all debts from the smallest to the largest current debt balance, add values to data as tuple
-    for debt in debts:
-    # TODO: calculate the monthly payment of each debt
-    # TODO: calculate the monthly interest due of each debt
+    # NOTE: Iterating through each debt, ordered by current balance, and applying the additional contribution to each payment. 
+    # When the debt is paid off, the # of terms is recorded. 
+    # When calculating the next debt, the additional contribution is not applied until the previously recorded # of terms is reached.
+    # When this new debt is paid off, the terms from when the additional payment was added and until the debt was paid off are added to the recorded # of terms
+    # Repeat...
+    terms_until_additional_contributions = 0
+
+    for index, debt in enumerate(debts):
+        current_balance = float(debt[0])
+        monthly_payment = calculate_monthly_payment(debt[3], debt[1], debt[2])
+
+        schedule.append([])
+
+        # Calculating each term
+        while current_balance > 0:
+            monthly_principle, monthly_interest = calculate_monthly_contribution(monthly_payment, debt[0], debt[1])
+            print("Monthly principle", monthly_principle)
+
+            # Update current balance (avoid infinite loop)
+            current_balance = calculate_new_balance(
+                current_balance,
+                monthly_payment,
+                additional_contribution_amount,
+                index,
+                terms_until_additional_contributions,
+            )
+
+            schedule[index].append((
+                index + 1,
+                monthly_principle,
+                monthly_interest,
+                current_balance, 
+            ))
+            terms_until_additional_contributions += 1
+
+        return schedule
 
 def calculate_monthly_contribution(monthly_payment, current_loan_amount, interest):
-    amount_to_interest = current_loan_amount * (interest / 12)
-    amount_to_principle = float(monthly_payment) - amount_to_interest
+    amount_to_interest = Decimal(current_loan_amount * (interest / 12)).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+    amount_to_principle = Decimal(monthly_payment) - amount_to_interest
 
     return f"{amount_to_principle:.2f}", f"{amount_to_interest:.2f}"
 
@@ -31,15 +64,34 @@ def calculate_monthly_payment(original_loan_amount, interest, term_length_in_mon
 
     return f"{original_loan_amount * (formula_top / formula_bottom):.2f}"
 
+def calculate_new_balance(
+    original_balance,
+    monthly_principle,
+    additional_contribution_amount = 0.0,
+    current_index = 0,
+    terms_until_additional_contributions = 0,
+):
+    new_balance = original_balance - float(monthly_principle)
+
+    if additional_contribution_amount > 0 and current_index >= terms_until_additional_contributions:
+        new_balance -= additional_contribution_amount
+
+    print("current balance", new_balance)
+
+    return new_balance
+
 def display_amortization_schedule():
     data = [] # (monthly payment, interest due, principle due, new balance)
 
-    print(tabulate(
-        data, 
-        headers = ["monthly payment", "interest due", "principle due", "new balance"],
-        showindex = "always",
-        tablefmt = "github",
-    ))
+    # print(tabulate(
+    #     data, 
+    #     headers = ["monthly payment", "interest due", "principle due", "new balance"],
+    #     showindex = "always",
+    #     tablefmt = "github",
+    # ))
+
+def display_currency(currency):
+    return f"{currency:.2f}"
 
 def sort_debts(debts):
     # NOTE: sorts by the current balance
--- amortization_test.py
+++ amortization_test.py
@@ -4,6 +4,101 @@ import pytest
 from amortization import *
 
 class TestAmortization:
+    def test_calculate_amortization_schedule(self):
+        passing_tests = [
+            [
+                # Input
+               {
+                    "additional_contribution_amount": 0,
+                    "additional_contribution_frequency": "monthly",
+                    "debts": [
+                        (100, .1, 12, 1000),
+                    ],
+                },
+                # Output
+                [
+                    # Debt schedule #1
+                    [
+                        # Payments
+                        (1, 79.58, 8.33, 920.42),
+                        (2, 80.25, 7.67, 840.17),
+                        (3, 80.91, 7.00, 759.26),
+                        (4, 81.59, 6.33, 677.67),
+                        (5, 82.27, 5.65, 595.40),
+                        (6, 82.95, 4.96, 512.45),
+                        (7, 83.65, 4.27, 428.80),
+                        (8, 84.34, 3.57, 344.46),
+                        (9, 85.05, 2.87, 259.41),
+                        (10, 85.75, 2.16, 173.66),
+                        (11, 86.47, 1.45, 87.19),
+                        (12, 87.19, 0.73, -0.00),
+                    ],
+                ],
+            ]
+        ]
+
+        for test in passing_tests:
+            assert calculate_amortization_schedule(test[0]) == test[1]
+
+    def test_calculate_monthly_payment(self):
+        passing_tests = [
+            (30000, .03, 48, "664.03"),
+            (1000, .1, 12, "87.92"),
+        ]
+
+        for test in passing_tests:
+            assert calculate_monthly_payment(test[0], test[1], test[2]) == test[3]
+
+    def test_calculate_monthly_contribution(self):
+        monthly_payment = calculate_monthly_payment(30000, .03, 48)
+        passing_tests = [
+            {
+                "parameters": [monthly_payment, 30000, .03],
+                "result": ["589.03", "75.00"],
+            },
+            {
+                "parameters": [monthly_payment, 29410.97, .03],
+                "result": ["590.50", "73.53"],
+            },
+            {
+                "parameters": [monthly_payment, 28820.47, .03],
+                "result": ["591.98", "72.05"],
+            },
+            {
+                "parameters": [87.92, 1000, .1],
+                "result": ["79.58", "8.33"],
+            },
+        ]
+
+        for test in passing_tests:
+            params = test["parameters"]
+            results = test["result"]
+            assert calculate_monthly_contribution(params[0], params[1], params[2]) == (results[0], results[1])
+
+    def test_calculate_new_balance(self):
+        passing_tests = [
+            # params: original balance, monthly principle, additional contribution amount, current index, terms until additional contributions
+            {
+                "params": [100, 10],
+                "result": 90.0,
+            },
+            {
+                "params": [100, 10, 10, 0, 0],
+                "result": 80.0,
+            },
+            {
+                "params": [100, 10, 10, 0, 1],
+                "result": 90.0,
+            },
+            {
+                "params": [100, 10, 10, 4, 1],
+                "result": 80.0,
+            },
+        ]
+
+        for test in passing_tests:
+            assert calculate_new_balance(*test["params"]) == test["result"]
+
     def test_sort_debts(self):
         passing_tests = [
             {
@@ -34,22 +129,3 @@ class TestAmortization:
                     break
 
                 assert debt[0] <= ordered_debts[index + 1][0]
-
-    def test_calculate_monthly_payment(self):
-        passing_tests = [
-            (30000, .03, 48, "664.03"),
-        ]
-
-        for test in passing_tests:
-            assert calculate_monthly_payment(test[0], test[1], test[2]) == test[3]
-
-    def test_calculate_monthly_contribution(self):
-        monthly_payment = calculate_monthly_payment(30000, .03, 48)
-        passing_tests = [
-            (monthly_payment, 30000, .03, "589.03", "75.00"),
-            (monthly_payment, 29410.97, .03, "590.50", "73.53"),
-            (monthly_payment, 28820.47, .03, "591.98", "72.05"),
-        ]
-
-        for test in passing_tests:
-            assert calculate_monthly_contribution(test[0], test[1], test[2]) == (test[3], test[4])
