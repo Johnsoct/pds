6bf7ffe3f16aad29b630c82772ae5232ea0fa833
Taylor <johnsoct@hey.com>
2025-08-31 17:28:58 -0400

    delete python files

--- main.py
+++
@@ -1,9 +0,0 @@
-# Modules
-import amortization
-import input as user_input
-
-def main():
-    debts_and_additional_contributions = user_input.main()
-    amortization.main(debts_and_additional_contributions)
-
-main()
