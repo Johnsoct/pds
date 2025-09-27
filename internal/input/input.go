addb5ab2733ca0bb6f71fe7731125a2a705e8ef6
Taylor <johnsoct@hey.com>
2025-09-27 04:34:12 -0400

    WIP on updating input python fns to go

--- internal/input/input.go
+++ internal/input/input.go
@@ -55,115 +55,116 @@ func castToDecimal(str string) (float64, error) {
 	return f, error
 }
 
-// def collect_additional_contribution_information(testing=False) -> (AdditionalContributionFrequency, AdditionalContributionAmount):
-//     print()
-//
-//     additional_contribution_frequency = collect_input(
-//         "frequency",
-//         "How often do you want to contribute an additional amount to your debt? ",
-//         testing=testing
-//     )
-//     additional_contribution_amount = collect_input(
-//         "numerical",
-//         f"How much would you like to contribute every {
-//             additional_contribution_frequency}? ",
-//         testing=testing
-//     )
-//
-//     return additional_contribution_amount, additional_contribution_frequency
-//
-//
+// TEST:
+func collectAdditionalContributionInformation(testing bool) (AdditionalContributionFrequency, AdditionalContributionAmount) {
+	fmt.Println()
+
+	additionalContributionFrequency := collectInput(
+		"frequency",
+		"How often do you want to contribute an additional amount to your debt?",
+		testing,
+	)
+	additionalContributionAmount := collectInput(
+		"numerical",
+		"How much would you like to contribute every"+"additional_contribution_frequency?",
+		testing,
+	)
+
+	return additionalContributionFrequency, additionalContributionAmount
+}
+
 // def collect_debt_information() -> DebtInformation:
-//     print()
-//     current_balance = collect_input(
-//         "numerical", "What is the current balance of the loan? ")
-//     interest_rate = collect_input(
-//         "numerical", "What is the interest rate of the loan? ")
-//     loan_amount = collect_input(
-//         "numerical", "What was the original amount of the loan? ")
-//     term_length = collect_input(
-//         "numerical", "What is the term length of the loan? ")
 //
-//     return current_balance, interest_rate, loan_amount, term_length
+//	print()
+//	current_balance = collectInput(
+//	    "numerical", "What is the current balance of the loan? ")
+//	interest_rate = collectInput(
+//	    "numerical", "What is the interest rate of the loan? ")
+//	loan_amount = collectInput(
+//	    "numerical", "What was the original amount of the loan? ")
+//	term_length = collectInput(
+//	    "numerical", "What is the term length of the loan? ")
 //
+//	return current_balance, interest_rate, loan_amount, term_length
 //
 // def confirm_additional_contribution_information(amount: Decimal, frequency: AdditionalContributionFrequency, testing=False) -> bool:
-//     print()
-//     display_additional_contribution_information((amount, frequency))
-//     print("---------------------------------------")
-//     print("Does this information look correct?")
-//     print("---------------------------------------")
-//     print("If 'NO', you'll be asked to enter the information again.")
-//     print("If 'YES', you'll move on to calculating your amortization schedule.")
 //
-//     user_input_confirmation = collect_input("confirmation", testing=testing)
+//	print()
+//	display_additional_contribution_information((amount, frequency))
+//	print("---------------------------------------")
+//	print("Does this information look correct?")
+//	print("---------------------------------------")
+//	print("If 'NO', you'll be asked to enter the information again.")
+//	print("If 'YES', you'll move on to calculating your amortization schedule.")
 //
-//     return get_user_confirmation_comparison(user_input_confirmation)
+//	user_input_confirmation = collectInput("confirmation", testing=testing)
 //
+//	return get_user_confirmation_comparison(user_input_confirmation)
 //
 // def confirm_additional_contribution_intent(testing=False) -> bool:
-//     print()
-//     print(f"Do you intend to contribute an additional amount towards your debt every {
-//           C.FREQUENCIES}")
-//     print("---------------------------------------")
-//     print("If 'YES', I'll ask you how much and how often")
-//     print("If 'NO', I'll continue to calculating your debt's amortization schedule")
-//     print("---------------------------------------")
 //
-//     additional_contribution_confirmation = collect_input(
-//         "confirmation", testing=testing)
+//	print()
+//	print(f"Do you intend to contribute an additional amount towards your debt every {
+//	      C.FREQUENCIES}")
+//	print("---------------------------------------")
+//	print("If 'YES', I'll ask you how much and how often")
+//	print("If 'NO', I'll continue to calculating your debt's amortization schedule")
+//	print("---------------------------------------")
 //
-//     return get_user_confirmation_comparison(additional_contribution_confirmation)
+//	additional_contribution_confirmation = collectInput(
+//	    "confirmation", testing=testing)
 //
+//	return get_user_confirmation_comparison(additional_contribution_confirmation)
 //
 // def confirm_additional_debt_intent(testing=False) -> bool:
-//     print()
-//     print("Would you like to enter another debt?")
-//     print("---------------------------------------")
-//     print("If 'NO', you'll move on towards getting your debt's amortization schedule.")
-//     print("If 'YES', you'll be asked to enter another debt's information.")
 //
-//     user_input_confirmation = collect_input("confirmation", testing=testing)
+//	print()
+//	print("Would you like to enter another debt?")
+//	print("---------------------------------------")
+//	print("If 'NO', you'll move on towards getting your debt's amortization schedule.")
+//	print("If 'YES', you'll be asked to enter another debt's information.")
 //
-//     return get_user_confirmation_comparison(user_input_confirmation)
+//	user_input_confirmation = collectInput("confirmation", testing=testing)
 //
+//	return get_user_confirmation_comparison(user_input_confirmation)
 //
 // def confirm_debt_information(
-//     debt_information: DebtInformation,
-//     testing=False,
+//
+//	debt_information: DebtInformation,
+//	testing=False,
+//
 // ) -> bool:
-//     print()
-//     display_debt_information(debt_information)
-//     print("---------------------------------------")
-//     print("Does this information look correct?")
-//     print("---------------------------------------")
-//     print("If 'NO', you'll be asked to enter the information again.")
-//     print("If 'YES', you'll move on to adding additional debts, if any.")
 //
-//     user_input_confirmation = collect_input("confirmation", testing=testing)
+//	print()
+//	display_debt_information(debt_information)
+//	print("---------------------------------------")
+//	print("Does this information look correct?")
+//	print("---------------------------------------")
+//	print("If 'NO', you'll be asked to enter the information again.")
+//	print("If 'YES', you'll move on to adding additional debts, if any.")
 //
-//     return get_user_confirmation_comparison(user_input_confirmation)
+//	user_input_confirmation = collectInput("confirmation", testing=testing)
 //
-//
-// def collect_input(type: str, prompt="", testing=False) -> NormalizedUserInput:
-//     options = get_options(type)
-//     user_input = input(prompt)
-//     user_input_normalized = normalize_user_input(
-//         type,
-//         user_input,
-//         C.get_disallowed_dangerous_characters_regex()
-//     )
-//
-//     if not validate_input(type, user_input_normalized, options):
-//         print(f"{user_input!r} was not valid")
-//
-//         # NOTE: When testing failed cases, we want to avoid infinite recursion
-//         if not testing:
-//             return collect_input(type, prompt)
-//     else:
-//         return user_input_normalized
-//
-//
+//	return get_user_confirmation_comparison(user_input_confirmation)
+func collectInput(category string, prompt string, testing bool) NormalizedUserInput {
+	options = get_options(type)
+	user_input = input(prompt)
+	user_input_normalized = normalize_user_input(
+	    category,
+	    user_input,
+	    C.get_disallowed_dangerous_characters_regex()
+	)
+
+	if not validate_input(type, user_input_normalized, options):
+	    print(f"{user_input!r} was not valid")
+
+	    # NOTE: When testing failed cases, we want to avoid infinite recursion
+	    if not testing:
+	        return collectInput(type, prompt)
+	else:
+	    return user_input_normalized
+}
+
 // def display_additional_contribution_information(additional_contribution_information: AdditionalContributionInformation):
 //     print()
 //     print()
